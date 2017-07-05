db6076f413aeea362999e3f1dbe56c20a8526365
Vadzim Dambrouski <vadzim.dambrouski@promwad.com>
2017-07-05 15:19:13 +0300

    Remove unused features.

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,9 @@
 //! - Data structures like the vector table
 //! - Safe wrappers around assembly instructions like `nop`
 
-#![feature(abi_msp430_interrupt)]
 #![deny(missing_docs)]
 #![deny(warnings)]
 #![feature(asm)]
-#![feature(const_fn)]
-#![feature(naked_functions)]
 #![no_std]
 
 extern crate mcu;
