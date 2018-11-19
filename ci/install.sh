424efd45a479eedc064a8c3c2796774425b4557d
Jorge Aparicio <jorge@japaric.io>
2018-11-19 23:48:25 +0100

    install p7zip

--- ci/install.sh
+++ ci/install.sh
@@ -10,7 +10,7 @@ main() {
 
             local file=msp430-gcc-7.3.2.154_linux64.7z
             curl -LO http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/exports/$file
-            7za x $file
+            7zr x $file
             ;;
     esac
 }
