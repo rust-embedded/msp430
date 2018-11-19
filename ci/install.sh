9178c6c67d3163725f285793fa39d1273a3ccf24
Jorge Aparicio <jorge@japaric.io>
2018-11-19 23:41:49 +0100

    install msp430-elf-gcc and enable the msp430-none-elf target

--- ci/install.sh
+++ ci/install.sh
@@ -7,6 +7,10 @@ main() {
                 cargo install xargo
 
             rustup component add rust-src
+
+            local file=msp430-gcc-7.3.2.154_linux64.7z
+            curl -LO http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/exports/$file
+            7za x $file
             ;;
     esac
 }
