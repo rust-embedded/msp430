ad715143fec2af6a005988a7479285d5d058849d
William D. Jones <thor0505@comcast.net>
2020-01-02 16:04:31 -0500

    Fix link to msp430 gcc toolchain in CI.

--- ci/install.sh
+++ ci/install.sh
@@ -8,9 +8,8 @@ main() {
 
             rustup component add rust-src
 
-            local file=msp430-gcc-7.3.2.154_linux64.7z
-            curl -LO http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/exports/$file
-            7zr x $file
+            curl -LO http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/8_3_1_0/export/msp430-gcc-8.3.1.25_linux64.tar.bz2
+            tar xjf msp430-gcc-8.3.1.25_linux64.tar.bz2
             ;;
     esac
 }
